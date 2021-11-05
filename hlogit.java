35be7f5d22530e4adc1adcedb8757b5686f32b42
abhicodex <abhishek.technophile>
2021-11-05 13:45:17 +0530

    Changed a to 20

--- hlogit.java
+++ hlogit.java
@@ -1,7 +1,7 @@
 class hlogit {
     public static void main(String[] args) {
-        int a = 43;
+        int a = 20;
         int b = 34;
-        System.out.println(a+b);
+        System.out.println(a + b);
     }
 }
